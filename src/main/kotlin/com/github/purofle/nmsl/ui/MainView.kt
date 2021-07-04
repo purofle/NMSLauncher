949eebe6eeba306e937beb2707ab279c06af46c7
purofle <3272912942@qq.com>
2021-07-04 09:28:40 +0800

    新界面

--- src/main/kotlin/com/github/purofle/nmsl/ui/MainView.kt
+++ src/main/kotlin/com/github/purofle/nmsl/ui/MainView.kt
@@ -2,19 +2,84 @@ package com.github.purofle.nmsl.ui
 
 import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.*
 import java.awt.datatransfer.UnsupportedFlavorException
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, DelicateCoroutinesApi::class)
 fun mainView() = application {
     setDefaultExceptionHandler()
     Window(
         title = "NMSL-Launcher"
     ) {
         DesktopMaterialTheme {
+            val scope = rememberCoroutineScope()
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(
+                scaffoldState = scaffoldState,
+                drawerContent = {
+
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                    }
+
+                },
+
+                //标题栏区域
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = "NMSL-Launcher") },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                        scope.launch {
+                                            scaffoldState.drawerState.open()
+                                        }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    )
+                },
+
+                //悬浮按钮
+                floatingActionButton = {
+                    ExtendedFloatingActionButton(
+                        text = { Text("悬浮按钮") },
+                        onClick = { }
+                    )
+                },
+                floatingActionButtonPosition = FabPosition.End,
+
+                //屏幕内容区域
+                content= {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "屏幕内容区域")
+                    }
+                },
+            )
 
         }
     }
@@ -28,9 +93,18 @@ private fun setDefaultExceptionHandler() {
             return@setDefaultUncaughtExceptionHandler
         }
         println(exception.stackTraceToString())
-        Window {
-            SelectionContainer {
-                Text(exception.stackTraceToString())
+        Window(
+            title = "出现错误"
+        ) {
+                Column {
+                    Button(
+                        {},
+                        Modifier.fillMaxWidth(),
+                        content = { Text("反馈") }
+                    )
+                    SelectionContainer {
+                        Text(exception.stackTraceToString())
+                }
             }
         }
     }
