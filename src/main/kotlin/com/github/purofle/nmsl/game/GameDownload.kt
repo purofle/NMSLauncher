99b8de286b13d6131526d4c84d8a4cc822e6b20f
purofle <3272912942@qq.com>
2021-07-04 15:14:33 +0800

    log等级设置

--- src/main/kotlin/com/github/purofle/nmsl/game/GameDownload.kt
+++ src/main/kotlin/com/github/purofle/nmsl/game/GameDownload.kt
@@ -7,20 +7,26 @@ import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import java.io.File
 
+/**
+ * 下载游戏相关文件
+ * @param path 文件下载目录
+ * @param logLevel log 输出等级
+ */
 class GameDownload(
-    path: File
+    path: File,
+    logLevel: LogLevel
 ) {
-    private val versionPath = File(path, "version")
-
+    private val versionPath = File(path, "version_manifest.json")
     private val client = HttpClient(CIO) {
         install(Logging) {
             logger = Logger.DEFAULT
-            level = LogLevel.BODY
+            level = logLevel
         }
     }
-    init {
-        path.check(true)
-    }
+
+    /**
+     * 下载并保存
+     */
     suspend fun getVersionManifest(): McVersionManifest {
         val resp = client.get<McVersionManifest>(
             "https://launchermeta.mojang.com/mc/game/version_manifest.json"
