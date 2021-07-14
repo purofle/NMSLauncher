d154337bb63eb4f1ec470952d5806cc9950dec18
purofle <3272912942@qq.com>
2021-07-14 16:36:56 +0800

    格式化时间，换成button

--- src/main/kotlin/com/github/purofle/nmsl/ui/view/DownloadView.kt
+++ src/main/kotlin/com/github/purofle/nmsl/ui/view/DownloadView.kt
@@ -2,20 +2,20 @@ package com.github.purofle.nmsl.ui.view
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.github.purofle.nmsl.game.Version
+import com.github.purofle.nmsl.utils.date
 
 @Composable
 //下载view
 fun DownloadView(data: List<Version>) {
     Box(
         modifier = Modifier.fillMaxSize()
-            .background(color = Color(180, 180, 180))
             .padding(10.dp)
     ) {
         val state = rememberScrollState(0)
@@ -27,7 +27,9 @@ fun DownloadView(data: List<Version>) {
                 .padding(end = 12.dp)
         ) {
             data.forEach {
-                TextBox("${it.id} - 发布时间：${it.releaseTime}")
+                TextBox("${it.id} - 发布时间：${it.releaseTime.date()}") {
+                    TODO("找不到版本： ${it.id}")
+                }
                 Spacer(modifier = Modifier.height(5.dp))
             }
         }
@@ -40,14 +42,18 @@ fun DownloadView(data: List<Version>) {
 }
 
 @Composable
-fun TextBox(text: String = "Item") {
-    Box(
-        modifier = Modifier.height(32.dp)
-            .fillMaxWidth()
-            .background(color = Color(0, 0, 0, 20))
-            .padding(start = 10.dp),
-        contentAlignment = Alignment.CenterStart
-    ) {
-        Text(text = text)
-    }
+fun TextBox(text: String = "Item", onClick: () -> Unit) {
+        Button(
+            onClick = onClick,
+            modifier = Modifier.background(Color(0, 0, 0,0))
+        ) {
+            Box(
+                modifier = Modifier.height(32.dp)
+                    .fillMaxWidth()
+                    .background(color = Color(0, 0, 0, 20))
+                    .padding(start = 10.dp), contentAlignment = Alignment.CenterStart
+            ) {
+                Text(text = text)
+            }
+        }
 }
