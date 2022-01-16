2fbe7315407f575c6767cef55a35eec4f95d74e0
purofle <3272912942@qq.com>
2022-01-16 21:52:55 +0800

    新增：Log系统

---
+++ src/main/kotlin/com/github/purofle/nmsl/utils/Log.kt
@@ -0,0 +1,9 @@
+package com.github.purofle.nmsl.utils
+
+import org.slf4j.Logger
+import org.slf4j.Logger.ROOT_LOGGER_NAME
+import org.slf4j.LoggerFactory
+
+object Log {
+    val logger: Logger = LoggerFactory.getLogger(ROOT_LOGGER_NAME)
+}
