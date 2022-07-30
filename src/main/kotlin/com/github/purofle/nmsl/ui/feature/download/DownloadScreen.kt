a4865a4bfcfb4b3fda893e2463a988af409a3732
purofle <purofle@gmail.com>
2022-07-30 22:56:59 +0800

    TODO: DownloadInfo

--- src/main/kotlin/com/github/purofle/nmsl/ui/feature/download/DownloadScreen.kt
+++ src/main/kotlin/com/github/purofle/nmsl/ui/feature/download/DownloadScreen.kt
@@ -7,53 +7,46 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.router.Router
 import com.github.purofle.nmsl.ui.component.VersionCard
+import com.github.purofle.nmsl.ui.navigation.Component
+import com.github.purofle.nmsl.ui.navigation.NavHostComponent
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun DownloadScreen(
-    viewModel: DownloadViewModel
+    viewModel: DownloadViewModel,
+    router: Router<NavHostComponent.Config, Component>
 ) {
 
     val scope = rememberCoroutineScope()
     val versions by viewModel.versions.collectAsState()
-    val searchDialog by viewModel.searchDialog.collectAsState()
+    var textValue by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
             SmallTopAppBar(
                 title = { Text("游戏下载") },
-                actions = {
-                    TextButton({ viewModel.showSearch() }) { Text("搜索") }
-                }
             )
         }
     ) { pd ->
 
         val state = rememberLazyListState()
 
-        if (searchDialog) {
-            AlertDialog(
-                title = { Text("搜索") },
-                text = { Text("请输入搜索关键字") },
-                confirmButton = {
-                    TextButton({ }) { Text("搜索") }
-                },
-                onDismissRequest = { viewModel.showSearch() }
-            )
-        }
-
         Column(modifier = Modifier.padding(pd)) {
+
+            TextField(
+                value = textValue,
+                onValueChange = { textValue = it },
+                label = { Text("Enter text") },
+                maxLines = 1,
+            )
 
             if (versions.isEmpty()) {
 
@@ -65,22 +58,24 @@ fun DownloadScreen(
             } else {
                 Box(modifier = Modifier.fillMaxSize()) {
                     // 把 versions 按照 release,snapshot, old_beta, old_aplha 的顺序排序
-                    val grouped = versions.sortedWith(
-                        compareBy(
-                            { it.type == "old_alpha" },
-                            { it.type == "old_beta" },
-                            { it.type == "snapshot" },
-                            { it.type == "release" },
+                    val grouped = versions
+                        .filter { textValue in it.id }
+                        .sortedWith(
+                            compareBy(
+                                { it.type == "old_alpha" },
+                                { it.type == "old_beta" },
+                                { it.type == "snapshot" },
+                                { it.type == "release" },
+                            )
                         )
-                    ).groupBy { it.type }
-
+                        .groupBy { it.type }
                     LazyColumn(state = state) {
                         grouped.forEach { (initial, contactsForInitial) ->
                             stickyHeader {
                                 SmallTopAppBar(title = { Text(initial) })
                             }
                             items(contactsForInitial) {
-                                VersionCard(it)
+                                VersionCard(it, router)
                             }
                         }
                     }
