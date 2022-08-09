04865a0954319d17edfefbcdd7241aeb72017e4b
purofle <purofle@gmail.com>
2022-08-10 01:06:10 +0800

    perf: 使用 Log4j2

--- src/main/kotlin/com/github/purofle/nmsl/main.kt
+++ src/main/kotlin/com/github/purofle/nmsl/main.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.window.application
 import com.github.purofle.nmsl.ui.feature.MainView
 import com.kieronquinn.monetcompat.compose.MonetCompatDynamicTheme
 import com.kieronquinn.monetcompat.core.MonetCompat
-import org.slf4j.LoggerFactory
+import org.apache.logging.log4j.LogManager
 
 fun main() {
     application {
@@ -13,7 +13,8 @@ fun main() {
             title = "NMSLauncher",
             onCloseRequest = ::exitApplication
         ) {
-            val logger = LoggerFactory.getLogger("NMSLauncher")
+
+            val logger = LogManager.getLogger(this::class.java)
             logger.debug("Starting NMSLauncher")
             logger.debug("seedColor: ${seedColor().red * 255}, ${seedColor().green * 255}, ${seedColor().blue * 255}")
             MonetCompatDynamicTheme(MonetCompat(seedColor())) {
