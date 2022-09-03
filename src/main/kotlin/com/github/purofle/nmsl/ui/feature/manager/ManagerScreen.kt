0758de0eeb68b20c00094d420692fdc20831fca6
purofle <purofle@gmail.com>
2022-09-03 22:08:53 +0800

    water: a commit

--- src/main/kotlin/com/github/purofle/nmsl/ui/feature/manager/ManagerScreen.kt
+++ src/main/kotlin/com/github/purofle/nmsl/ui/feature/manager/ManagerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.*
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.github.purofle.nmsl.game.GameJson
 
 //下载游戏view
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,7 +40,20 @@ fun ManagerScreen(
                         Text("暂无游戏, 请前往下载页面下载")
                     }
                 }
+
+                items(versionManifest) { gameJson ->
+                    VersionUI(gameJson)
+                }
             }
         }
+    }
+}
+
+
+@Composable
+fun VersionUI(gameJson: GameJson) {
+    Column {
+        Text(gameJson.id)
+        Text(gameJson.releaseTime)
     }
 }
