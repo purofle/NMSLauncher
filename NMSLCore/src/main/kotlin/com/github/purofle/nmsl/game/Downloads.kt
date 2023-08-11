c5cefdf34896b90ae58e01ef71bc670214ebefaf
purofle <purofle@gmail.com>
2023-08-11 20:36:56 +0800

    reflector: 不好意思，没杀干净

--- NMSLCore/src/main/kotlin/com/github/purofle/nmsl/game/Downloads.kt
+++ NMSLCore/src/main/kotlin/com/github/purofle/nmsl/game/Downloads.kt
@@ -1,7 +1,6 @@
 package com.github.purofle.nmsl.game
 
 
-import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,12 +11,9 @@ data class Downloads(
 
 @Serializable
 data class Client(
-    @SerializedName("sha1")
-    val sha1: String = "", // 0f275bc1547d01fa5f56ba34bdc87d981ee12daf
-    @SerializedName("size")
-    val size: Int = 0, // 10180113
-    @SerializedName("url")
-    val url: String = "" // https://launcher.mojang.com/v1/objects/0f275bc1547d01fa5f56ba34bdc87d981ee12daf/client.jar
+    val sha1: String, // 0f275bc1547d01fa5f56ba34bdc87d981ee12daf
+    val size: Int, // 10180113
+    val url: String // https://launcher.mojang.com/v1/objects/0f275bc1547d01fa5f56ba34bdc87d981ee12daf/client.jar
 )
 
 @Serializable
