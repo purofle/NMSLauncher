6c54862c59cb21a9133d163f7a89f4535c0ac232
purofle <purofle@gmail.com>
2023-08-13 01:02:07 +0800

    reflector(ui): 修改主页面ui

--- src/main/kotlin/com/github/purofle/nmsl/pages/MainPage.kt
+++ src/main/kotlin/com/github/purofle/nmsl/pages/MainPage.kt
@@ -1,46 +1,109 @@
 package com.github.purofle.nmsl.pages
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Gamepad
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
-    val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
-    val selectedItem = remember { mutableStateOf(items[0]) }
-
     Scaffold {
-
         PermanentNavigationDrawer(
             drawerContent = {
-                PermanentDrawerSheet(Modifier.width(200.dp)) {
-                    Column {
-                        Text("NMSLauncher")
+                PermanentDrawerSheet(Modifier.width(220.dp)) {
+                    Column(modifier = Modifier.padding(horizontal = 28.dp)) {
+                        Text("NMSLauncher", modifier = Modifier.padding(17.dp))
                         NavigationDrawerItem(
                             icon = { Icon(Icons.Default.Gamepad, contentDescription = null) },
                             label = { Text("Game") },
                             selected = true,
                             onClick = {
                             },
-                            modifier = Modifier.padding(horizontal = 0.dp)
+                        )
+                        NavigationDrawerItem(
+                            icon = { Icon(Icons.Default.CloudDownload, contentDescription = null) },
+                            label = { Text("Download") },
+                            selected = false,
+                            onClick = {
+                            },
+                        )
+                        Divider()
+                        Text("Launcher", modifier = Modifier.padding(17.dp))
+                        NavigationDrawerItem(
+                            icon = { Icon(Icons.Default.Settings, contentDescription = null) },
+                            label = { Text("Settings") },
+                            selected = false,
+                            onClick = {
+                            },
+                        )
+                        NavigationDrawerItem(
+                            icon = { Icon(Icons.Default.Flag, contentDescription = null) },
+                            label = { Text("About") },
+                            selected = false,
+                            onClick = {
+                            },
                         )
                     }
                 }
             },
             content = {
-
+                Row {
+                    Column(
+                        modifier = Modifier.width(250.dp).fillMaxHeight()
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        OutlinedTextField(
+                            "",
+                            {},
+                            modifier = Modifier.padding(11.dp),
+                            label = { Text("Search Games") },
+                            leadingIcon = { Icon(Icons.Default.Search, "") },
+                            shape = RoundedCornerShape(30.dp),
+                            singleLine = true
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.clickable { }.fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.onSecondary)
+                        ) {
+                            val primaryContainer = MaterialTheme.colorScheme.primaryContainer
+                            Text("F", modifier = Modifier.drawBehind {
+                                drawCircle(primaryContainer, radius = 20f)
+                            }.padding(30.dp, 15.dp))
+                            Text("Fabric-1.20.1", modifier = Modifier.padding(5.dp, 0.dp))
+                        }
+                        repeat(5) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.clickable { }.fillMaxWidth()
+                            ) {
+                                val primaryContainer = MaterialTheme.colorScheme.primaryContainer
+                                Text("A", modifier = Modifier.drawBehind {
+                                    drawCircle(primaryContainer, radius = 20f)
+                                }.padding(30.dp, 15.dp))
+                                Text("List item", modifier = Modifier.padding(5.dp, 0.dp))
+                            }
+                        }
+                    }
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = painterResource("nanami.jpg"),
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
             }
         )
     }
