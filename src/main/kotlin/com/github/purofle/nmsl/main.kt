7b307b85aa32eec5cb2552758c4d3efd20539d74
purofle <purofle@gmail.com>
2023-08-12 22:57:47 +0800

    delete.

--- src/main/kotlin/com/github/purofle/nmsl/main.kt
+++ src/main/kotlin/com/github/purofle/nmsl/main.kt
@@ -2,7 +2,7 @@ package com.github.purofle.nmsl
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.github.purofle.nmsl.ui.feature.MainView
+import com.github.purofle.nmsl.pages.MainPage
 import com.kieronquinn.monetcompat.compose.MonetCompatDynamicTheme
 import com.kieronquinn.monetcompat.core.MonetCompat
 import org.apache.logging.log4j.LogManager
@@ -18,11 +18,8 @@ fun main() {
             title = "NMSLauncher",
             onCloseRequest = ::exitApplication
         ) {
-			val logger = LogManager.getLogger(this::class.java)
-            // My dream starts here.
-            logger.debug("seedColor: ${seedColor().red * 255}, ${seedColor().green * 255}, ${seedColor().blue * 255}")
             MonetCompatDynamicTheme(MonetCompat(seedColor())) {
-                MainView()
+                MainPage()
             }
         }
     }
--- src/main/kotlin/com/github/purofle/nmsl/ui/feature/account/AddAccountDialog.kt
+++
@@ -1,8 +0,0 @@
-package com.github.purofle.nmsl.ui.feature.account
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AddAccount() {
-
-}
