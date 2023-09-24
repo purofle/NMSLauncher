ab91ec431a12f5df9f83cf9d1345091b81253e8a
purofle <purofle@gmail.com>
2023-09-24 11:31:49 +0800

    refactor: check game

--- NMSLCore/src/main/kotlin/com/github/purofle/nmsl/config/NmslConfig.kt
+++ NMSLCore/src/main/kotlin/com/github/purofle/nmsl/config/NmslConfig.kt
@@ -1,11 +1,19 @@
 package com.github.purofle.nmsl.config
 
 import com.github.purofle.nmsl.auth.microsoft.MinecraftProfile
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class NmslConfig(
+    val msa: Msa,
+    val profile: MinecraftProfile
+)
+
+@Serializable
+data class Msa(
     val accessToken: String,
     val refreshToken: String,
-    val profile: MinecraftProfile
+    @SerialName("expires_in")
+    val expiresIn: Int,
 )
