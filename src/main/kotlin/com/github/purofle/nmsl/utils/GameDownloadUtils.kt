d528c38cfdccc1de28f438ea805b6e7790c62f7a
purofle <purofle@gmail.com>
2023-12-27 00:30:00 +0800

    GameDownloadUtils: 实现版本隔离

--- src/main/kotlin/com/github/purofle/nmsl/utils/GameDownloadUtils.kt
+++ src/main/kotlin/com/github/purofle/nmsl/utils/GameDownloadUtils.kt
@@ -4,6 +4,8 @@ import com.github.purofle.nmsl.config.Config
 import com.github.purofle.nmsl.download.DownloadProvider
 import com.github.purofle.nmsl.download.GameDownloader
 import com.github.purofle.nmsl.game.GameManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 
 fun DownloadProvider.Companion.getDefaultProvider(): DownloadProvider {
@@ -12,23 +14,31 @@ fun DownloadProvider.Companion.getDefaultProvider(): DownloadProvider {
 }
 
 fun getGameDownloader(gameVersion: String): GameDownloader {
-    val config = Config.config
     val provider = DownloadProvider.getDefaultProvider()
     val manifest = GameManager.getLocalManifest()
     return GameDownloader(provider, manifest.versions.first { it.id == gameVersion })
 }
 
-fun startGame(launcherArgument: List<String>) {
-    println(launcherArgument.joinToString(" "))
-    val sh = File.createTempFile("nmsl", "sh")
-    sh.writeText("java " + launcherArgument.joinToString(" "))
-    sh.deleteOnExit()
+suspend fun startGame(version: String, launcherArgument: List<String>) {
 
-    ProcessBuilder(
-        "/bin/sh", sh.absolutePath
-    )
-        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-        .redirectError(ProcessBuilder.Redirect.INHERIT)
-        .start()
-        .waitFor()
+    with(getGameDownloader(version)) {
+        downloadLibrary()
+        downloadAssets()
+        downloadClientJar()
+    }
+
+    println(launcherArgument.joinToString(" "))
+    withContext(Dispatchers.IO) {
+        val sh = File.createTempFile("nmsl", "sh")
+        sh.writeText("java " + launcherArgument.joinToString(" "))
+        sh.deleteOnExit()
+
+        ProcessBuilder(
+            "/bin/sh", sh.absolutePath
+        )
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .redirectError(ProcessBuilder.Redirect.INHERIT)
+            .start()
+            .waitFor()
+    }
 }
