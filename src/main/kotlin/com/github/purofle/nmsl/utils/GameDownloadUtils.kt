7f55cd67fb71b06337c3c326e17e608c1c5e3568
purofle <purofle@gmail.com>
2023-12-31 22:09:42 +0800

    GameDownloadUtils: add windows start game

--- src/main/kotlin/com/github/purofle/nmsl/utils/GameDownloadUtils.kt
+++ src/main/kotlin/com/github/purofle/nmsl/utils/GameDownloadUtils.kt
@@ -4,6 +4,7 @@ import com.github.purofle.nmsl.config.Config
 import com.github.purofle.nmsl.download.DownloadProvider
 import com.github.purofle.nmsl.download.GameDownloader
 import com.github.purofle.nmsl.game.GameManager
+import com.github.purofle.nmsl.utils.os.OS
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -27,18 +28,27 @@ suspend fun startGame(version: String, launcherArgument: List<String>) {
         downloadClientJar()
     }
 
-    println(launcherArgument.joinToString(" "))
-    withContext(Dispatchers.IO) {
-        val sh = File.createTempFile("nmsl", "sh")
-        sh.writeText("java " + launcherArgument.joinToString(" "))
-        sh.deleteOnExit()
-
-        ProcessBuilder(
-            "/bin/sh", sh.absolutePath
-        )
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor()
+    if (OS.CURRENT_OS != OS.WINDOWS) {
+        startGameUnix(launcherArgument)
+    } else {
+        startGameWindows(launcherArgument)
     }
+}
+
+suspend fun startGameUnix(launcherArgument: List<String>): Unit = withContext(Dispatchers.IO) {
+    val sh = File.createTempFile("nmsl", "sh")
+    sh.writeText("java " + launcherArgument.joinToString(" "))
+    sh.deleteOnExit()
+
+    ProcessBuilder(
+        "/bin/sh", sh.absolutePath
+    )
+        .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start()
+        .waitFor()
+}
+
+suspend fun startGameWindows(launcherArgument: List<String>): Unit = withContext(Dispatchers.IO) {
+    TODO(launcherArgument.joinToString(" "))
 }
